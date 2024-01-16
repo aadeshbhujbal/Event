7d7fcda1c0dd0d506f5d529549eba958a15a136b
Aadesh Santosh Bhujbal <aadeshbhujbal1@gmail.com>
2024-01-16 06:45:50 +0530

    project inititation complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,26 @@
-import "styles/tailwind.css"
+import "./globals.css"
+
+import type { Metadata } from "next"
+import { Poppins } from "next/font/google"
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+})
+
+export const metadata: Metadata = {
+  title: "Evently",
+  description: "Evently is a platform for event management.",
+  icons: {
+    icon: "/assets/images/logo.svg",
+  },
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={poppins.variable}>{children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next"
-import { Button } from "@/components/Button/Button"
+import { Button } from "@/components/ui/button"
 
 export const metadata: Metadata = {
   title: "Next.js Enterprise Boilerplate",
@@ -18,25 +18,14 @@ export const metadata: Metadata = {
   },
 }
 
-/**
- * Renders a web component that displays an event driven, AI-powered, and highly scalable web development section.
- *
- * @return {JSX.Element} The rendered web component.
- */
-
-
 export default function Web() {
   return (
     <>
-      <section className="bg-white dark:bg-gray-900">
-        <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
-          <div className="mx-auto place-self-center">
-            <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
-              Event driven, AI-powered, and highly scalable web development.
-            </h1>
-          </div>
-        </div>
-      </section>
+      <main>
+        <Button variant="default" className="px-10">
+          Click Me
+        </Button>
+      </main>
     </>
   )
 }
